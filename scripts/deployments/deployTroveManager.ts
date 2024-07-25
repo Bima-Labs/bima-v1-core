17964c3e9222200420e99e3a2994fba227b77969
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2024-07-25 21:41:53 +0400

    scripts

--- scripts/deployments/deployTroveManager.ts
+++ scripts/deployments/deployTroveManager.ts
@@ -4,7 +4,6 @@ const ZERO_ADDRESS = ethers.ZeroAddress;
 
 // FILL IN WITH YOUR TARGET ADDRESSES
 const COLLATERAL_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const AGGREGATOR_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const FACTORY_ADDRESS = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
 const PRICEFEED_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 const BABELVAULT_ADDRESS = "0x68B1D87F95878fE05B998F19b66F4baba5De1aed";
@@ -17,12 +16,24 @@ async function main() {
     BABELVAULT_ADDRESS
   );
 
+  // const mockedStBtcFactory = await ethers.getContractFactory("StakedBTC");
+  // const mockedStBtc = await mockedStBtcFactory.deploy();
+  // await mockedStBtc.waitForDeployment();
+  // const mockedStBtcAddress = await mockedStBtc.getAddress();
+  // console.log("MOCKED stBTC deployed!: ", mockedStBtcAddress);
+
+  const mockOracleFactory = await ethers.getContractFactory("MockOracle");
+  const mockOracle = await mockOracleFactory.deploy();
+  await mockOracle.waitForDeployment();
+  const mockOracleAddress = await mockOracle.getAddress();
+  console.log("MockOracle deployed!: ", mockOracleAddress);
+
   console.log("troveManagerCount before: ", await factory.troveManagerCount());
 
   {
     const tx = await priceFeed.setOracle(
       COLLATERAL_ADDRESS,
-      AGGREGATOR_ADDRESS,
+      mockOracleAddress,
       BigInt("80000"),
       "0x00000000",
       BigInt("18"),
--- scripts/deployments/manageTroveManager.ts
+++ scripts/deployments/manageTroveManager.ts
@@ -9,6 +9,96 @@ const BORROWEROPERATIONS_ADDRESS = "0x0169148d7e9F7c415f4a3565383c803093A4C36A";
 async function main() {
   const [owner] = await ethers.getSigners();
 
+  const {
+    troveManager,
+    borrowerOperations,
+    debtToken,
+    collateralToken,
+    priceFeed,
+  } = await getContracts();
+
+  console.log("TroveManager DT: ", await troveManager.debtToken());
+  // console.log("CollateralToken: ", await troveManager.collateralToken());
+  // console.log("PriceFeed: ", await troveManager.priceFeed());
+  // console.log("MCR: ", await troveManager.MCR());
+
+  // console.log("BorrowerOperations DT: ", await borrowerOperations.debtToken());
+
+  // {
+  //   const tx = await borrowerOperations.getTCR();
+  //   await tx.wait();
+  //   console.log("TCR: ", tx.value);
+  // }
+
+  // console.log(
+  //   "Entire System Collateral: ",
+  //   await troveManager.getEntireSystemColl()
+  // );
+  // console.log("Entire System Debt: ", await troveManager.getEntireSystemDebt());
+  // console.log("Debt Token Total Supply: ", await debtToken.totalSupply());
+
+  // console.log(
+  //   "my troves nominal ICR: ",
+  //   await troveManager.getNominalICR(owner.address)
+  // );
+  // console.log(
+  //   "my troves current ICR: ",
+  //   await troveManager.getCurrentICR(
+  //     owner.address,
+  //     ethers.parseUnits("60000", 8)
+  //   )
+  // );
+
+  // console.log(
+  //   "My stBTC balance: ",
+  //   await collateralToken.balanceOf(owner.address)
+  // );
+
+  // {
+  //   const tx = await priceFeed.fetchPrice(collateralTokenAddress);
+  //   await tx.wait();
+  //   console.log("stBTC price: ", tx.value);
+  // }
+
+  // {
+  //   const tx = await troveManager.getEntireSystemBalances();
+  //   await tx.wait();
+  //   console.log(tx.);
+  // }
+
+  // {
+  //   console.log("APPROVING COLLATERAL TOKENS to BORROW OPERATIONS..");
+  //   const tx2 = await collateralToken.approve(
+  //     BORROWEROPERATIONS_ADDRESS,
+  //     parseUnits("1", 18)
+  //   );
+  //   await tx2.wait();
+  //   console.log("COLLATERAL TOKENS APPROVED!");
+  // }
+  // // Open Trove
+  // {
+  //   console.log("OPENING A TROVE..");
+  //   const tx = await borrowerOperations.openTrove(
+  //     TROVEMANAGER_ADDRESS,
+  //     owner.address,
+  //     parseUnits("1", 18),
+  //     parseUnits("1", 18),
+  //     parseUnits("44500", 18),
+  //     ZERO_ADDRESS,
+  //     ZERO_ADDRESS
+  //   );
+  //   await tx.wait();
+  //   console.log("TROVE OPENED!");
+  // }
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+const getContracts = async () => {
   const troveManager = await ethers.getContractAt(
     "TroveManager",
     TROVEMANAGER_ADDRESS
@@ -30,85 +120,11 @@ async function main() {
     "PriceFeed",
     await troveManager.priceFeed()
   );
-
-  // console.log("TroveManager DT: ", await troveManager.debtToken());
-  // console.log("CollateralToken: ", await troveManager.collateralToken());
-  // console.log("PriceFeed: ", await troveManager.priceFeed());
-  // console.log("MCR: ", await troveManager.MCR());
-
-  // console.log("BorrowerOperations DT: ", await borrowerOperations.debtToken());
-
-  // {
-  //   const tx = await borrowerOperations.getTCR();
-  //   await tx.wait();
-  //   console.log("TCR: ", tx.value);
-  // }
-
-  console.log(
-    "Entire System Collateral: ",
-    await troveManager.getEntireSystemColl()
-  );
-  console.log("Entire System Debt: ", await troveManager.getEntireSystemDebt());
-  console.log("Debt Token Total Supply: ", await debtToken.totalSupply());
-
-  console.log(
-    "my troves nominal ICR: ",
-    await troveManager.getNominalICR(owner.address)
-  );
-  console.log(
-    "my troves current ICR: ",
-    await troveManager.getCurrentICR(
-      owner.address,
-      ethers.parseUnits("60000", 8)
-    )
-  );
-
-  console.log(
-    "My stBTC balance: ",
-    await collateralToken.balanceOf(owner.address)
-  );
-
-  // {
-  //   const tx = await priceFeed.fetchPrice(collateralTokenAddress);
-  //   await tx.wait();
-  //   console.log("stBTC price: ", tx.value);
-  // }
-
-  // {
-  //   console.log("APPROVING COLLATERAL TOKENS to TROVE MANAGER..");
-  //   const tx1 = await collateralToken.approve(
-  //     TROVEMANAGER_ADDRESS,
-  //     parseUnits("1", 18)
-  //   );
-  //   await tx1.wait();
-  //   console.log("COLLATERAL TOKENS APPROVED!");
-  //   console.log("APPROVING COLLATERAL TOKENS to BORROW OPERATIONS..");
-  //   const tx2 = await collateralToken.approve(
-  //     BORROWEROPERATIONS_ADDRESS,
-  //     parseUnits("1", 18)
-  //   );
-  //   await tx2.wait();
-  //   console.log("COLLATERAL TOKENS APPROVED!");
-  // }
-  // // Open Trove
-  // {
-  //   console.log("OPENING A TROVE..");
-  //   const tx = await borrowerOperations.openTrove(
-  //     TROVEMANAGER_ADDRESS,
-  //     owner.address,
-  //     parseUnits("1", 18),
-  //     parseUnits("1", 18),
-  //     parseUnits("10000", 18),
-  //     ZERO_ADDRESS,
-  //     ZERO_ADDRESS
-  //   );
-  //   await tx.wait();
-  //   console.log("TROVE OPENED!");
-  // }
-}
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  return {
+    troveManager,
+    borrowerOperations,
+    debtToken,
+    collateralToken,
+    priceFeed,
+  };
+};
