b45e34c97f7f916d44c77084585ac4b15bbe90d0
Suraj Gyaneshwar Waghmare <waghmaresuraj159@gmail.com>
2024-07-31 23:33:35 +0530

    Deployed Core Contracts on Polygon Testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,23 +6,16 @@ import "@nomicfoundation/hardhat-chai-matchers";
 import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  // defaultNetwork: "polygon_amoy",
+  defaultNetwork: "gate_testnet",
 
   etherscan: {
     apiKey: {
-      lorenzo_testnet: "abc",
+      // lorenzo_testnet: "abc",
+      
+      // gate_testnet:"9063a1a8a3ee226fa440779bc6f7015a",
+      // bscTestnet:`CC7GINMBKSKVQZTXC9GD7M852K65CPEVRF`,
+      polygon_amoy:`WIXGNHX39425CPDKS5TFBDJBKMGTDWR7QI,`
     },
-    customChains: [
-      {
-        network: "lorenzo_testnet",
-        chainId: 83291,
-
-        urls: {
-          browserURL: "https://scan-testnet.lorenzo-protocol.xyz/",
-          apiURL: "https://scan-testnet.lorenzo-protocol.xyz/api/",
-        },
-      },
-    ],
   },
   networks: {
     lorenzo_testnet: {
@@ -31,10 +24,25 @@ const config: HardhatUserConfig = {
     },
     polygon_amoy: {
       url: "https://rpc-amoy.polygon.technology",
+      chainId:80002,
       accounts: [
-        "7c5b27c4f043051e405d03469e3f9dfe5b65df74376dcaf70db003d63a976efc",
+        "a0b8d309dd0afc95c94fc319f2de434f845a747d662c71ccbc08655c29299519",
+      ],
+      gasPrice: 25000000000, // 25 gwei in wei
+    },
+    gate_testnet:{
+      url: "https://meteora-evm.gatenode.cc",
+      chainId:85,
+      accounts:[
+        "ea5fbf909b888a1e810ef6ca42f06cb7eabc9a04095ec7e11fdeed7bb968ab07",
       ],
     },
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-2-s3.binance.org:8545/",
+      chainId:97,
+      accounts:[
+        "ea5fbf909b888a1e810ef6ca42f06cb7eabc9a04095ec7e11fdeed7bb968ab07",
+      ],},
   },
   solidity: {
     compilers: [
