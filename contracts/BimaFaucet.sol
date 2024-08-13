ef794d8d930c3fa4ba5f9b53fe67a42ca86129a6
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2024-08-13 17:31:18 +0400

    Faucet (#37)
    * faucet contract
    
    * faucet tests
    
    * ownable faucet

---
+++ contracts/BimaFaucet.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BimaFaucet is Ownable {
+  uint256 public constant TOKEN_AMOUNT = 2e18;
+  uint256 public constant INTERVAL = 1 days;
+
+  mapping(address => mapping(address => uint256)) public tokensRecievedAt; // account => token => timestamp
+
+  function getTokens(address _tokenAddress) external {
+    require(tokensRecievedAt[msg.sender][_tokenAddress] + INTERVAL < block.timestamp);
+
+    IERC20(_tokenAddress).transfer(msg.sender, TOKEN_AMOUNT);
+
+    tokensRecievedAt[msg.sender][_tokenAddress] = block.timestamp;
+  }
+
+  function recoverTokens(address _tokenAddress) external onlyOwner {
+    IERC20(_tokenAddress).transfer(msg.sender, IERC20(_tokenAddress).balanceOf(address(this)));
+  }
+}
---
+++ test/Faucet.test.ts
@@ -0,0 +1,196 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { parseEther } from "ethers";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+
+describe("Faucet", function () {
+  it("Tokens should be recoverable by owner", async function () {
+    const [owner, user1, user2] = await ethers.getSigners();
+
+    const ERC20Deployer = await ethers.getContractFactory("StakedBTC");
+    const FaucetDeployer = await ethers.getContractFactory("BimaFaucet");
+
+    const stBTC1 = await ERC20Deployer.deploy();
+    const stBTC2 = await ERC20Deployer.deploy();
+
+    const stBTC1Address = await stBTC1.getAddress();
+    const stBTC2Address = await stBTC2.getAddress();
+
+    const faucet = await FaucetDeployer.deploy();
+    const faucetAddress = await faucet.getAddress();
+
+    expect(await stBTC1.balanceOf(owner)).to.equal(parseEther("1000000"));
+    expect(await stBTC2.balanceOf(owner)).to.equal(parseEther("1000000"));
+
+    await stBTC1.transfer(faucetAddress, parseEther("1000"));
+    await stBTC2.transfer(faucetAddress, parseEther("2000"));
+
+    expect(await stBTC1.balanceOf(owner)).to.equal(parseEther("999000"));
+    expect(await stBTC2.balanceOf(owner)).to.equal(parseEther("998000"));
+
+    await expect(faucet.connect(user1).recoverTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user2).recoverTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user1).recoverTokens(stBTC2Address)).to.be.reverted;
+    await expect(faucet.connect(user2).recoverTokens(stBTC2Address)).to.be.reverted;
+
+    await faucet.recoverTokens(stBTC1Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("0"));
+    expect(await stBTC1.balanceOf(owner)).to.equal(parseEther("1000000"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("2000"));
+    expect(await stBTC2.balanceOf(owner)).to.equal(parseEther("998000"));
+
+    await faucet.recoverTokens(stBTC2Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("0"));
+    expect(await stBTC1.balanceOf(owner)).to.equal(parseEther("1000000"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("0"));
+    expect(await stBTC2.balanceOf(owner)).to.equal(parseEther("1000000"));
+  });
+  it("Faucet should work as expected", async function () {
+    const [owner, user1, user2] = await ethers.getSigners();
+
+    const ERC20Deployer = await ethers.getContractFactory("StakedBTC");
+    const FaucetDeployer = await ethers.getContractFactory("BimaFaucet");
+
+    const stBTC1 = await ERC20Deployer.deploy();
+    const stBTC2 = await ERC20Deployer.deploy();
+
+    const stBTC1Address = await stBTC1.getAddress();
+    const stBTC2Address = await stBTC2.getAddress();
+
+    const faucet = await FaucetDeployer.deploy();
+    const faucetAddress = await faucet.getAddress();
+
+    await stBTC1.transfer(faucetAddress, parseEther("1000"));
+    await stBTC2.transfer(faucetAddress, parseEther("2000"));
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("1000"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("2000"));
+
+    expect(await stBTC1.balanceOf(user1.address)).to.equal(parseEther("0"));
+    expect(await stBTC2.balanceOf(user1.address)).to.equal(parseEther("0"));
+
+    expect(await stBTC1.balanceOf(user2.address)).to.equal(parseEther("0"));
+    expect(await stBTC2.balanceOf(user2.address)).to.equal(parseEther("0"));
+
+    await faucet.connect(user1).getTokens(stBTC1Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("998"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("2000"));
+
+    expect(await stBTC1.balanceOf(user1.address)).to.equal(parseEther("2"));
+    expect(await stBTC2.balanceOf(user1.address)).to.equal(parseEther("0"));
+
+    expect(await stBTC1.balanceOf(user2.address)).to.equal(parseEther("0"));
+    expect(await stBTC2.balanceOf(user2.address)).to.equal(parseEther("0"));
+
+    await expect(faucet.connect(user1).getTokens(stBTC1Address)).to.be.reverted;
+
+    await faucet.connect(user1).getTokens(stBTC2Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("998"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("1998"));
+
+    expect(await stBTC1.balanceOf(user1.address)).to.equal(parseEther("2"));
+    expect(await stBTC2.balanceOf(user1.address)).to.equal(parseEther("2"));
+
+    expect(await stBTC1.balanceOf(user2.address)).to.equal(parseEther("0"));
+    expect(await stBTC2.balanceOf(user2.address)).to.equal(parseEther("0"));
+
+    await expect(faucet.connect(user1).getTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user1).getTokens(stBTC2Address)).to.be.reverted;
+
+    await time.increase(86390);
+
+    await expect(faucet.connect(user1).getTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user1).getTokens(stBTC2Address)).to.be.reverted;
+
+    await faucet.connect(user2).getTokens(stBTC2Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("998"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("1996"));
+
+    expect(await stBTC1.balanceOf(user1.address)).to.equal(parseEther("2"));
+    expect(await stBTC2.balanceOf(user1.address)).to.equal(parseEther("2"));
+
+    expect(await stBTC1.balanceOf(user2.address)).to.equal(parseEther("0"));
+    expect(await stBTC2.balanceOf(user2.address)).to.equal(parseEther("2"));
+
+    await time.increase(120);
+
+    await faucet.connect(user1).getTokens(stBTC1Address);
+    await faucet.connect(user1).getTokens(stBTC2Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("996"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("1994"));
+
+    expect(await stBTC1.balanceOf(user1.address)).to.equal(parseEther("4"));
+    expect(await stBTC2.balanceOf(user1.address)).to.equal(parseEther("4"));
+
+    expect(await stBTC1.balanceOf(user2.address)).to.equal(parseEther("0"));
+    expect(await stBTC2.balanceOf(user2.address)).to.equal(parseEther("2"));
+
+    await expect(faucet.connect(user2).getTokens(stBTC2Address)).to.be.reverted;
+
+    await faucet.connect(user2).getTokens(stBTC1Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("994"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("1994"));
+
+    expect(await stBTC1.balanceOf(user1.address)).to.equal(parseEther("4"));
+    expect(await stBTC2.balanceOf(user1.address)).to.equal(parseEther("4"));
+
+    expect(await stBTC1.balanceOf(user2.address)).to.equal(parseEther("2"));
+    expect(await stBTC2.balanceOf(user2.address)).to.equal(parseEther("2"));
+
+    await expect(faucet.connect(user2).getTokens(stBTC1Address)).to.be.reverted;
+
+    await expect(faucet.connect(user1).getTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user1).getTokens(stBTC2Address)).to.be.reverted;
+
+    await time.increase(86400);
+
+    await faucet.connect(user1).getTokens(stBTC1Address);
+    await faucet.connect(user1).getTokens(stBTC2Address);
+    await faucet.connect(user2).getTokens(stBTC1Address);
+    await faucet.connect(user2).getTokens(stBTC2Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("990"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("1990"));
+
+    expect(await stBTC1.balanceOf(user1.address)).to.equal(parseEther("6"));
+    expect(await stBTC2.balanceOf(user1.address)).to.equal(parseEther("6"));
+
+    expect(await stBTC1.balanceOf(user2.address)).to.equal(parseEther("4"));
+    expect(await stBTC2.balanceOf(user2.address)).to.equal(parseEther("4"));
+
+    await expect(faucet.connect(user1).getTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user1).getTokens(stBTC2Address)).to.be.reverted;
+    await expect(faucet.connect(user2).getTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user2).getTokens(stBTC2Address)).to.be.reverted;
+
+    await time.increase(86300);
+
+    await expect(faucet.connect(user1).getTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user1).getTokens(stBTC2Address)).to.be.reverted;
+    await expect(faucet.connect(user2).getTokens(stBTC1Address)).to.be.reverted;
+    await expect(faucet.connect(user2).getTokens(stBTC2Address)).to.be.reverted;
+
+    await time.increase(200);
+
+    await faucet.connect(user1).getTokens(stBTC1Address);
+    await faucet.connect(user1).getTokens(stBTC2Address);
+    await faucet.connect(user2).getTokens(stBTC1Address);
+    await faucet.connect(user2).getTokens(stBTC2Address);
+
+    expect(await stBTC1.balanceOf(faucetAddress)).to.equal(parseEther("986"));
+    expect(await stBTC2.balanceOf(faucetAddress)).to.equal(parseEther("1986"));
+
+    expect(await stBTC1.balanceOf(user1.address)).to.equal(parseEther("8"));
+    expect(await stBTC2.balanceOf(user1.address)).to.equal(parseEther("8"));
+
+    expect(await stBTC1.balanceOf(user2.address)).to.equal(parseEther("6"));
+    expect(await stBTC2.balanceOf(user2.address)).to.equal(parseEther("6"));
+  });
+});
