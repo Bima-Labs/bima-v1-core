00890b41fe403e992d13b140ae7506154363668e
moayaan1911 <ayaangames@gmail.com>
2024-08-14 22:11:50 +0530

    faucet contract new functions

--- contracts/BimaFaucet.sol
+++ contracts/BimaFaucet.sol
@@ -1,24 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BimaFaucet is Ownable {
-  uint256 public constant TOKEN_AMOUNT = 2e18;
+  uint256 public tokenAmount = 2e18;
   uint256 public constant INTERVAL = 1 days;
 
   mapping(address => mapping(address => uint256)) public tokensRecievedAt; // account => token => timestamp
 
+  constructor() Ownable(msg.sender) {}
+
   function getTokens(address _tokenAddress) external {
     require(tokensRecievedAt[msg.sender][_tokenAddress] + INTERVAL < block.timestamp);
 
-    IERC20(_tokenAddress).transfer(msg.sender, TOKEN_AMOUNT);
+    IERC20(_tokenAddress).transfer(msg.sender, tokenAmount);
 
     tokensRecievedAt[msg.sender][_tokenAddress] = block.timestamp;
   }
 
   function recoverTokens(address _tokenAddress) external onlyOwner {
     IERC20(_tokenAddress).transfer(msg.sender, IERC20(_tokenAddress).balanceOf(address(this)));
+  }
+
+  function redeemAllTokens(address _tokenAddress) external onlyOwner {
+    uint256 balance = IERC20(_tokenAddress).balanceOf(address(this));
+    IERC20(_tokenAddress).transfer(msg.sender, balance);
+  }
+
+  function updateTokenAmount(uint256 _newAmount) external onlyOwner {
+    tokenAmount = _newAmount;
   }
 }
