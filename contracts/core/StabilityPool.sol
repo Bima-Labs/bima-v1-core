f4285e544ae4af8f8d78b02531c3379be0c7fae9
DevDacian <114125154+devdacian@users.noreply.github.com>
2024-08-19 17:02:22 +0900

    fix license

--- contracts/core/StabilityPool.sol
+++ contracts/core/StabilityPool.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MITIBabelVault
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
