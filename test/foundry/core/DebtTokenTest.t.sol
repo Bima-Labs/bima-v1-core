13272da97551a3fc814506ff9607081a33b85b4f
DevDacian <114125154+devdacian@users.noreply.github.com>
2024-09-24 20:00:41 +0900

    adding fuzz test for DebtToken::flashLoan

--- test/foundry/core/DebtTokenTest.t.sol
+++ test/foundry/core/DebtTokenTest.t.sol
@@ -4,7 +4,9 @@ pragma solidity 0.8.19;
 // test setup
 import {TestSetup, DebtToken} from "../TestSetup.sol";
 
-contract DebtTokenTest is TestSetup {
+import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+
+contract DebtTokenTest is IERC3156FlashBorrower, TestSetup {
 
     function test_flashLoanFee() external {
         // entire supply initially available to borrow
@@ -27,6 +29,28 @@ contract DebtTokenTest is TestSetup {
         borrowAmount = 1111;
         vm.expectRevert("ERC20FlashMint: amount too small");
         debtToken.flashFee(borrowAmount);
+    }
 
+    function test_flashLoan(uint256 amount) external {
+        amount = bound(amount, 1e18, 1_000_000_000_000e18);
+
+        debtToken.flashLoan(this, amount, bytes(""));
+    }
+
+    function onFlashLoan(
+        address /*initiator*/,
+        address /*token*/,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata /*data*/
+    ) external returns (bytes32 returnVal) {
+        returnVal = keccak256("ERC3156FlashBorrower.onFlashLoan");
+
+        // mint ourselves the fee
+        vm.prank(address(borrowerOps));
+        debtToken.mint(address(this), fee);
+
+        // approve debt token contract to take amount + fee
+        debtToken.approve(address(debtToken), amount+fee);
     }
 }
