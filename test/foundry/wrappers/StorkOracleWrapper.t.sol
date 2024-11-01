97c0f27b196af201e2b33df1a22e8405e879ea9e
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2024-11-01 15:14:36 +0400

    StorkOracleWrapper: move tests to foundry/wrappers

--- test/foundry/wrappers/StorkOracleWrapper.t.sol
+++ test/foundry/wrappers/StorkOracleWrapper.t.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.19;
 import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
 
-import {StorkOracleWrapper, IStorkOracle} from "../../contracts/wrappers/StorkOracleWrapper.sol";
-import {IAggregatorV3Interface} from "../../contracts/interfaces/IAggregatorV3Interface.sol";
-import {PriceFeed} from "../../contracts/core/PriceFeed.sol";
-import {IBorrowerOperations} from "../../contracts/interfaces/IBorrowerOperations.sol";
-import {ITroveManager} from "../../contracts/interfaces/ITroveManager.sol";
+import {StorkOracleWrapper, IStorkOracle} from "../../../contracts/wrappers/StorkOracleWrapper.sol";
+import {IAggregatorV3Interface} from "../../../contracts/interfaces/IAggregatorV3Interface.sol";
+import {PriceFeed} from "../../../contracts/core/PriceFeed.sol";
+import {IBorrowerOperations} from "../../../contracts/interfaces/IBorrowerOperations.sol";
+import {ITroveManager} from "../../../contracts/interfaces/ITroveManager.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract StorkOracleWrapperTest is Test {
