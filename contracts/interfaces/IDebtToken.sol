a865bb623a81c5f9afe77873255fbbc8bf06c43e
0xminer11 <arali.naveen1@gmail.com>
2025-03-19 23:46:28 +0530

    updated the Interface for debtToken and bimaTokencontracts

--- contracts/interfaces/IDebtToken.sol
+++ contracts/interfaces/IDebtToken.sol
@@ -14,8 +14,26 @@ interface IDebtToken {
     event SetTrustedRemoteAddress(uint16 _remoteChainId, bytes _remoteAddress);
     event SetUseCustomAdapterParams(bool _useCustomAdapterParams);
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event PreCrimeSet(address preCrimeAddress);
+    event PeerSet(uint32 eid, bytes32 peer);
+
+    event OFTSent(
+        bytes32 indexed guid, 
+        uint32 dstEid,
+        address indexed fromAddress,
+        uint256 amountSentLD,
+        uint256 amountReceivedLD
+    );
+    event OFTReceived(
+        bytes32 indexed guid,
+        uint32 srcEid,
+        address indexed toAddress,
+        uint256 amountReceivedLD
+    );
 
     function approve(address spender, uint256 amount) external returns (bool);
+
+    function authorizedMint(address _to, uint256 _amount) external;
 
     function burn(address _account, uint256 _amount) external;
 
@@ -27,22 +45,29 @@ interface IDebtToken {
 
     function flashLoan(address receiver, uint256 amount, bytes calldata data) external returns (bool);
 
-    function forceResumeReceive(uint16 _srcChainId, bytes calldata _srcAddress) external;
-
     function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
 
-    function lzReceive(uint16 _srcChainId, bytes calldata _srcAddress, uint64 _nonce, bytes calldata _payload) external;
+    function lzReceive(
+        Origin calldata _origin,
+        bytes32 _guid,
+        bytes calldata _message,
+        address _executor,
+        bytes calldata _extraData
+    ) external payable;
+
+    function lzReceiveAndRevert(InboundPacket[] calldata _packets) external payable;
+
+    function _lzReceiveSimulate(
+        Origin calldata _origin,
+        bytes32 _guid,
+        bytes calldata _message,
+        address _executor,
+        bytes calldata _extraData
+    ) internal;
 
     function mint(address _account, uint256 _amount) external;
 
     function mintWithGasCompensation(address _account, uint256 _amount) external returns (bool);
-
-    function nonblockingLzReceive(
-        uint16 _srcChainId,
-        bytes calldata _srcAddress,
-        uint64 _nonce,
-        bytes calldata _payload
-    ) external;
 
     function permit(
         address owner,
@@ -58,25 +83,25 @@ interface IDebtToken {
 
     function returnFromPool(address _poolAddress, address _receiver, uint256 _amount) external;
 
+    function send(
+        SendParam calldata _sendParam,
+        MessagingFee calldata _fee,
+        address _refundAddress
+    ) external payable returns (MessagingReceipt memory, OFTReceipt memory);
+
     function sendToSP(address _sender, uint256 _amount) external;
 
-    function setConfig(uint16 _version, uint16 _chainId, uint256 _configType, bytes calldata _config) external;
+    function setDelegate(address _delegate) external;
 
-    function setMinDstGas(uint16 _dstChainId, uint16 _packetType, uint256 _minGas) external;
+    function setEnforcedOptions(EnforcedOptionParam[] calldata _enforcedOptions) external;
 
-    function setPayloadSizeLimit(uint16 _dstChainId, uint256 _size) external;
+    function setLendingVaultAdapterAddress(address _lendingVaultAdapterAddress) external;
+
+    function setMsgInspector(address _msgInspector) public;
+
+    function setPeer(uint32 _eid, bytes32 _peer) external;
 
     function setPrecrime(address _precrime) external;
-
-    function setReceiveVersion(uint16 _version) external;
-
-    function setSendVersion(uint16 _version) external;
-
-    function setTrustedRemote(uint16 _srcChainId, bytes calldata _path) external;
-
-    function setTrustedRemoteAddress(uint16 _remoteChainId, bytes calldata _remoteAddress) external;
-
-    function setUseCustomAdapterParams(bool _useCustomAdapterParams) external;
 
     function transfer(address recipient, uint256 amount) external returns (bool);
 
@@ -84,93 +109,79 @@ interface IDebtToken {
 
     function transferOwnership(address newOwner) external;
 
-    function retryMessage(
-        uint16 _srcChainId,
-        bytes calldata _srcAddress,
-        uint64 _nonce,
-        bytes calldata _payload
-    ) external payable;
-
-    function sendFrom(
-        address _from,
-        uint16 _dstChainId,
-        bytes calldata _toAddress,
-        uint256 _amount,
-        address _refundAddress,
-        address _zroPaymentAddress,
-        bytes calldata _adapterParams
-    ) external payable;
-
-    function DEBT_GAS_COMPENSATION() external view returns (uint256);
-
-    function DEFAULT_PAYLOAD_SIZE_LIMIT() external view returns (uint256);
-
-    function FLASH_LOAN_FEE() external view returns (uint256);
-
-    function NO_EXTRA_GAS() external view returns (uint256);
-
-    function PT_SEND() external view returns (uint16);
-
     function allowance(address owner, address spender) external view returns (uint256);
+
+    function allowInitializePath(Origin calldata _origin) external view returns (bool);
+
+    function approvalRequired() external view returns (bool);
 
     function balanceOf(address account) external view returns (uint256);
 
     function borrowerOperationsAddress() external view returns (address);
 
-    function circulatingSupply() external view returns (uint256);
+    function combineOptions(
+        uint32 _eid,
+        uint16 _msgType,
+        bytes calldata _extraOptions
+    ) external view returns (bytes memory options);
+
+    function DEBT_GAS_COMPENSATION() external view returns (uint256);
 
     function decimals() external view returns (uint8);
 
     function domainSeparator() external view returns (bytes32);
 
-    function estimateSendFee(
-        uint16 _dstChainId,
-        bytes calldata _toAddress,
-        uint256 _amount,
-        bool _useZro,
-        bytes calldata _adapterParams
-    ) external view returns (uint256 nativeFee, uint256 zroFee);
+    function endpoint() external view returns (ILayerZeroEndpointV2 iEndpoint);
+
+    // enforcedOptions
 
     function factory() external view returns (address);
 
-    function failedMessages(uint16, bytes calldata, uint64) external view returns (bytes32);
+    function FLASH_LOAN_FEE() external view returns (uint256);
 
-    function flashFee(uint256 amount) external pure returns (uint256);
+    function flashFee(address token, uint256 amount) external view returns (uint256);
 
     function gasPool() external view returns (address);
 
-    function getConfig(
-        uint16 _version,
-        uint16 _chainId,
-        address,
-        uint256 _configType
-    ) external view returns (bytes memory);
+    function isComposeMsgSender(
+        Origin calldata _origin,
+        bytes calldata _message,
+        address _sender
+    ) external view returns (bool isSender);
 
-    function getTrustedRemoteAddress(uint16 _remoteChainId) external view returns (bytes memory);
+    function isPeer(uint32 _eid, bytes32 _peer) external view returns (bool);
 
-    function isTrustedRemote(uint16 _srcChainId, bytes calldata _srcAddress) external view returns (bool);
-
-    function lzEndpoint() external view returns (address);
-
-    function maxFlashLoan() external view returns (uint256);
-
-    function minDstGasLookup(uint16, uint16) external view returns (uint256);
+    function maxFlashLoan(address token) public view returns (uint256);
 
     function name() external view returns (string memory);
 
+    function nextNonce(uint32 _eid, bytes32 _sender) external view returns (uint64);
+
     function nonces(address owner) external view returns (uint256);
+
+    function oApp() external view returns (address);
+
+    function oAppVersion() external view returns (uint64 senderVersion, uint64 receiverVersion);
+
+    function oftVersion() external view returns (bytes4 interfaceId, uint64 version);
 
     function owner() external view returns (address);
 
-    function payloadSizeLimitLookup(uint16) external view returns (uint256);
+    function peers(uint32 _eid) external view returns (bytes32 peer);
 
     function permitTypeHash() external view returns (bytes32);
 
-    function precrime() external view returns (address);
+    function preCrime() external view returns (address);
+
+    function quoteOFT(
+        SendParam calldata _sendParam
+    ) external view returns (OFTLimit memory, OFTFeeDetail[] memory oftFeeDetails, OFTReceipt memory);
+
+    function quoteSend(SendParam calldata _sendParam, bool _payInLzToken) external view returns (MessagingFee memory);
+
+    function sharedDecimals() external view returns (uint8);
 
     function stabilityPoolAddress() external view returns (address);
-
-    function supportsInterface(bytes4 interfaceId) external view returns (bool);
 
     function symbol() external view returns (string memory);
 
@@ -180,9 +191,4 @@ interface IDebtToken {
 
     function troveManager(address) external view returns (bool);
 
-    function trustedRemoteLookup(uint16) external view returns (bytes memory);
-
-    function useCustomAdapterParams() external view returns (bool);
-
     function version() external view returns (string memory);
-}
