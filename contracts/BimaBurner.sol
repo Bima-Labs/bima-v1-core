f29de756b15c7cc51675ee9e09c7e23da485626e
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2025-05-13 11:56:46 +0700

    BimaBurner

--- contracts/BimaBurner.sol
+++ contracts/BimaBurner.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-// Contract were all the assets will be sent which was unable to sent to address(0)
+// The contract, for burning assets, mainly USBD
 contract BimaBurner {}
